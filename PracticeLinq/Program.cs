86ac3c6a84a02bcc78489e082410ea9a6bc3540a
Jake Hammond <jakehammond226@gmail.com>
2022-07-20 18:31:14 -0500

    done

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,5 @@
+﻿List<string> videoGames = new List<string> { "Fortnite", "Warzone", "Apex", "Rocket League", "Minecraft" };
+
+videoGames.OrderBy(x => x.Length).ToList().ForEach(game => Console.WriteLine(game));
+
+
